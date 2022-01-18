18e23c027a53b5305cfad4ba4c0762ed4421206e
Maciej Tomaszewicz <1994tomaszewicz@gmail.com>
2022-01-18 13:47:30 +0100

    new setting for heroku

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 
+const path = require('path')
 const connectDB = require("./config/db");
 const productRoutes = require("./routes/productRoutes");
 const emailRoutes = require("./routes/emailRoutes");
@@ -8,7 +9,7 @@ const emailRoutes = require("./routes/emailRoutes");
 connectDB();
 
 const app = express();
-var cors = require("cors");
+
 
 app.use(express.json());
 
@@ -19,7 +20,17 @@ app.use(express.urlencoded());
 
 app.use("/api/emails", emailRoutes);
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
 
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    })
+} else {
+    app.get("/", (req, res) => {
+        res.send("Api running");
+    })
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
